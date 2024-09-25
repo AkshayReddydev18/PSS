8610a17847cddf648fdb38f32add75f2313148fa
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-09-25 11:10:53 +0530

    2509

---
+++ 2509/batterflystarpattern.js
@@ -0,0 +1,19 @@
+// rows=+prompt("enter the rows:");
+rows=7;
+for(i=1;i<=2*rows-1;i++){
+    stars=(i<=rows)?i:i-(i-rows)*2;
+    sp=(i<rows)?(rows-i)*2-1:2*(i-rows)-1;
+    sp=(sp<0)?0:sp
+    res="";
+    for(j=1;j<=stars;j++){
+        // if(j%2==0)
+        // res+="* ";
+        // else
+        res+="* "
+    }
+    if(i==rows){
+        console.log(res+res.slice(2,res.length))
+    }else{
+    console.log(res+"  ".repeat(sp)+res);
+    }
+}
